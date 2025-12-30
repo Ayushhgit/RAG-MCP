f581eb043ba24f8f033d7dda12c419c259ff2235
Ayush <ayushraj2005.muz@gmail.com>
2025-12-30 19:14:58 +0530

    chunking function

---
+++ app/core/chunking.py
@@ -0,0 +1,11 @@
+def chunk_text(text: str, chunk_size: int, overlap: int) -> list[str]:
+    words = text.split()
+    chunks = []
+
+    start = 0
+    while start < len(words):
+        end = start + chunk_size
+        chunk = words[start:end]
+        chunks.append(" ".join(chunk))
+        start = end - overlap
+    return chunks
